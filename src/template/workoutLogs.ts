13bf9523b29a557811657abbeb1015f993c7fb65
uuta <nightrage.metal66@gmail.com>
2024-07-07 19:59:52 +0900

    Refactor workoutTitles to include category

--- src/template/workoutLogs.ts
+++ src/template/workoutLogs.ts
@@ -1,25 +1,75 @@
 import { createForm } from '../form';
 import { addHeaders, createSheet } from '../sheet';
 
+type WorkoutEntities = {
+  category: string;
+  youtubeUrl: string;
+};
+
 /**
  * Add keys and values whatever you want to use in the form
  */
-const workoutTitles: Record<string, string> = {
-  'Incline dumbbell press': '8iPEnn-ltC8&ab_channel=ScottHermanFitness',
-  'Incline dumbbell fly': 'JSDpq14vCZ8&ab_channel=PureGym',
-  'Chest press': 'xUm0BiZCWlQ&ab_channel=ScottHermanFitness',
-  'Hip thrust': 'SEdqd1n0cvg&ab_channel=ScottHermanFitness',
-  'Leg press': 'qCR9bN3G1t4&ab_channel=PureGym',
-  'Inner Thigh': 'CjAVezAggkI&ab_channel=PureGym',
-  'Outer Thigh': 'G_8LItOiZ0Q&ab_channel=PureGym',
-  'Arnold press': 'jeJttN2EWCo&ab_channel=PureGym',
-  'Rear raise': 'nlkF7_2O_Lw&ab_channel=PureGym',
-  'Side raise': 'z-kOn7flIZg&ab_channel=PureGym',
-  'Front raise': 'zkP0MsTcIVU&ab_channel=PureGym',
-  'Incline side raise': 'Ak8RCI35y40&ab_channel=JoshBowers',
-  'Knee to chest': '9hVZ4rc2_3Y&ab_channel=LIVESTRONG.COM',
-  'Close Press': 'fZuQpjhaR_M&ab_channel=PureGym',
-  'Hyght dumbbell fly': 'ADDBZPi6Up8&ab_channel=SETFORSET',
+const workoutTitles: Record<string, WorkoutEntities> = {
+  'Incline dumbbell press': {
+    category: 'chest',
+    youtubeUrl: '8iPEnn-ltC8&ab_channel=ScottHermanFitness',
+  },
+  'Incline dumbbell fly': {
+    youtubeUrl: 'JSDpq14vCZ8&ab_channel=PureGym',
+    category: 'chest',
+  },
+  'Chest press': {
+    youtubeUrl: 'xUm0BiZCWlQ&ab_channel=ScottHermanFitness',
+    category: 'chest',
+  },
+  'Hip thrust': {
+    youtubeUrl: 'SEdqd1n0cvg&ab_channel=ScottHermanFitness',
+    category: 'butt',
+  },
+  'Leg press': {
+    youtubeUrl: 'qCR9bN3G1t4&ab_channel=PureGym',
+    category: 'legs',
+  },
+  'Inner Thigh': {
+    youtubeUrl: 'CjAVezAggkI&ab_channel=PureGym',
+    category: 'legs',
+  },
+  'Outer Thigh': {
+    youtubeUrl: 'G_8LItOiZ0Q&ab_channel=PureGym',
+    category: 'legs',
+  },
+  'Arnold press': {
+    youtubeUrl: 'jeJttN2EWCo&ab_channel=PureGym',
+    category: 'shoulder',
+  },
+  'Rear raise': {
+    youtubeUrl: 'nlkF7_2O_Lw&ab_channel=PureGym',
+    category: 'shoulder',
+  },
+  'Side raise': {
+    youtubeUrl: 'z-kOn7flIZg&ab_channel=PureGym',
+    category: 'shoulder',
+  },
+  'Front raise': {
+    youtubeUrl: 'zkP0MsTcIVU&ab_channel=PureGym',
+    category: 'shoulder',
+  },
+  'Incline side raise': {
+    youtubeUrl: 'Ak8RCI35y40&ab_channel=JoshBowers',
+    category: 'shoulder',
+  },
+  'Knee to chest': {
+    youtubeUrl: '9hVZ4rc2_3Y&ab_channel=LIVESTRONG.COM',
+    category: 'stomach',
+  },
+  'Close Press': {
+    youtubeUrl: 'fZuQpjhaR_M&ab_channel=PureGym',
+    category: 'chest',
+  },
+  'Hyght dumbbell fly': {
+    youtubeUrl: 'ADDBZPi6Up8&ab_channel=SETFORSET',
+    category: 'chest',
+  },
 };
 
 type SheetEntities = {
@@ -42,10 +92,14 @@ const workoutSheets: Record<string, SheetEntities> = {
 };
 
 export function workoutLogs() {
+  const entities: Record<string, string> = {};
+  for (const [key, value] of Object.entries(workoutTitles)) {
+    entities[key] = value.youtubeUrl;
+  }
   const { sheet } = createForm({
     name: 'Workout logs',
     description: 'Workout form for the gym. Please fill out the form below.',
-    entities: workoutTitles,
+    entities,
   });
   Object.entries(workoutSheets).forEach(([sheetName, v]) => {
     const sheetInstance = createSheet(sheet, sheetName);
